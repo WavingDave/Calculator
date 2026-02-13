8dd1abe8428b10259b14fc7529f1a95a6a4a6c17
WavingDave <davidleebecker1994@gmail.com>
2026-02-13 09:21:58 +0100

    changed eval funktion and made PI and % work. Also added EventListener for Keybord input.

--- index.js
+++ index.js
@@ -11,13 +11,28 @@ function clearDisplay() {
 }
 function calculate() {
   try {
-    display.value = eval(display.value);
+    let expression = display.value.replace(/π/g, Math.PI).replace(/%/g, "/100");
+
+    display.value = Function('"use strict"; return (' + expression + ")")();
   } catch (error) {
     display.value = "False input";
   }
 }
 
-console.log(piBtn);
-console.log(typeof piBtn);
+document.addEventListener("keydown", function (event) {
+  if (!isNaN(event.key) || "+-*/.%".includes(event.key)) {
+    appendToDisplay(event.key);
+  }
 
+  if (event.key === "Enter") {
+    calculate();
+  }
 
+  if (event.key === "Backspace") {
+    display.value = display.value.slice(0, -1);
+  }
+
+  if (event.key === "Escape") {
+    clearDisplay();
+  }
+});
